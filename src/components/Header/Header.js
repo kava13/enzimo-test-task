9216971b57e5bcddf9d1080a3bdeaa408c6d4c8c
Alexander <kava3331@mail.ru>
2020-10-15 18:27:22 +0300

    Added section Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../../css/interface.css';
+import '../../css/header.css';
+
+class Header extends React.Component {
+    render () {
+        return (
+            <header className='header'>
+                <div className='container'>
+                <div className='header-block'>
+                    <div className='header-logo'>
+                    <div className='header-logo__img'><img src="/images/logo-img.png"/></div>
+                    <div className='header-logo__text'><img src="/images/logo-text.png"/></div>
+                    </div>
+                    <div className='header-nav'>
+                        <button className='header-nav__search'><img src="/images/search.png"></img></button>
+                        <button className='header-nav__faq'>?</button>
+                    </div>
+                </div>     
+            </div>
+            </header>
+            
+        );
+    }
+}
+
+export default Header;
