ff83c28a2a39bc2af32822dcbcf1fc65fdc5a74b
Alexander <kava3331@mail.ru>
2020-10-16 01:35:40 +0300

    Changed App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
-import './css/interface.css'
-import Header from './components/Header/Header'
+import './css/interface.css';
+import Header from './components/Header/Header';
+import Doctors from './components/Doctors/Doctors';
 
-function App() {
-  return (
-    <div>
-      <Header/>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super()
+    this.state = { doctorsData: [] }
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData = async () => {
+    const response = await axios.get('/enzimo-data.json');
+    this.setState({ doctorsData: response.data})
+  }
+
+  render() {
+  
+    const { doctorsData } = this.state;
+    
+
+    return (
+      <div>
+        <Header/>
+        { <Doctors doctorsData={doctorsData}/> }
+      </div>
+    );
+  }
 }
 
 export default App;
